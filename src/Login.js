a11ef2b651f7e63444828a38849db2340e1ac410
dimmetrius <dimmetrius@gmail.com>
2017-08-28 13:05:54 +0300

    try login

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,35 @@ import { StyleSheet, Text, View, Linking, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
 
+function serialize(params, obj, traditional, scope) {
+  let type;
+  const array = false;
+  const hash = true;
+  const add = (arr, key, _value) => {
+    let value = _value;
+    if (value == null) value = '';
+    arr.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+  };
+  Object.keys(obj).forEach((value, _key) => {
+    let key = _key;
+    type = typeof value;
+    if (scope) {
+      key = traditional ? scope : `${scope}[${hash || type === 'object' || type === 'array' ? key : ''}]`;
+    }
+    // handle data in serializeArray() format
+    if (!scope && array) add(params, value.name, value.value);
+    else if (type === 'array' || (!traditional && type === 'object')) {
+      serialize(params, value, traditional, key);
+    } else add(params, key, value);
+  });
+}
+
+function param(obj, traditional) {
+  const params = [];
+  serialize(params, obj, traditional);
+  return params.join('&').replace(/%20/g, '+');
+}
+
 export default class Login extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
@@ -46,25 +75,38 @@ export default class Login extends Component {
     */
   };
 
-  loginWithFacebook = () => {
+  loginWithFacebook1 = () => {
     this.openURL('http://sportiq.io/signin/facebook');
   };
   // Handle Login with Facebook button tap
-  loginWithFacebook1 = () => {
+  loginWithFacebook = () => {
+    /*
+    const data = 'scope=publish_actions';
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+
+    xhr.open('POST', 'http://sportiq.io/signin/facebook');
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+    xhr.send(data);
+    */
+
+
     fetch('http://sportiq.io/signin/facebook', {
       method: 'POST',
-      redirect: 'error',
-
+      redirect: 'follow',
+      credentials: 'include',
       headers: {
-        Origin: 'http://sportiq.io',
-        'Upgrade-Insecure-Requests': 1,
+        'Accept': 'application/json',
         'Content-Type': 'application/x-www-form-urlencoded',
-        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        Referer: 'http://sportiq.io/',
-        // Accept-Encoding: gzip, deflate
-        // Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4
-        Cookie: 'language=ru;',
       },
+      body: 'scope=public_profile',
     })
       .then((data) => {
         console.log(data);
@@ -74,6 +116,7 @@ export default class Login extends Component {
         console.log(err);
       });
 
+    /*
     fetch('http://sportiq.io/loggedUser', {
       headers: {
         Cookie: 'language=ru; SESSION=3a0425be-d9d6-47b8-89dc-4e529d2b42f2;',
@@ -85,6 +128,7 @@ export default class Login extends Component {
       })
       .then(d => console.log(d))
       .catch(e => console.log(e));
+      */
     // this.openURL('http://sportiq.io/signin/facebook');
   };
 
