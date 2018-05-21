23666239888efc4611660abb37f4e8377f26fbc4
dimmetrius <dimmetrius@gmail.com>
2018-05-21 22:58:04 +0300

    build 16

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,16 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { autoRehydrate } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
 import appReducer from '../reducers';
 import mySaga from './../sagas';
-
-/*
-import { LOG_OUT } from './../actions';
-const rootReducer = (_state, action) => {
-  let state = _state;
-  if (action.type === LOG_OUT) {
-    Object.keys(state).forEach((key) => {
-      storage.removeItem(`persist:${key}`);
-    });
-    state = undefined;
-  }
-
-  return appReducer(state, action);
-};
-*/
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
 const store = compose(autoRehydrate())(createStore)(
   appReducer,
-  applyMiddleware(logger),
+  // applyMiddleware(logger),
   applyMiddleware(sagaMiddleware),
 );
 
